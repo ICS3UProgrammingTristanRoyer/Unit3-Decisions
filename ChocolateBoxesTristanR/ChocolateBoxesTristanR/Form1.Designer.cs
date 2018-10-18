214ac3fbf4237acb239eb86916a6892530127620
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-10-18 11:06:56 -0400

    ok

--- ChocolateBoxesTristanR/ChocolateBoxesTristanR/Form1.Designer.cs
+++ ChocolateBoxesTristanR/ChocolateBoxesTristanR/Form1.Designer.cs
@@ -28,12 +28,71 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.lblQuestion = new System.Windows.Forms.Label();
+			this.txtAnswer = new System.Windows.Forms.TextBox();
+			this.lblDisplay = new System.Windows.Forms.Label();
+			this.btnCheck = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblQuestion
+			// 
+			this.lblQuestion.AutoSize = true;
+			this.lblQuestion.Location = new System.Drawing.Point(2, 32);
+			this.lblQuestion.Name = "lblQuestion";
+			this.lblQuestion.Size = new System.Drawing.Size(202, 13);
+			this.lblQuestion.TabIndex = 0;
+			this.lblQuestion.Text = "How many chocolate boxes did you sell?:";
+			// 
+			// txtAnswer
+			// 
+			this.txtAnswer.Location = new System.Drawing.Point(200, 29);
+			this.txtAnswer.Name = "txtAnswer";
+			this.txtAnswer.Size = new System.Drawing.Size(82, 20);
+			this.txtAnswer.TabIndex = 1;
+			this.txtAnswer.TextChanged += new System.EventHandler(this.txtAnswer_TextChanged);
+			// 
+			// lblDisplay
+			// 
+			this.lblDisplay.AutoSize = true;
+			this.lblDisplay.Location = new System.Drawing.Point(120, 133);
+			this.lblDisplay.Name = "lblDisplay";
+			this.lblDisplay.Size = new System.Drawing.Size(13, 13);
+			this.lblDisplay.TabIndex = 2;
+			this.lblDisplay.Text = "--";
+			this.lblDisplay.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// btnCheck
+			// 
+			this.btnCheck.Location = new System.Drawing.Point(98, 81);
+			this.btnCheck.Name = "btnCheck";
+			this.btnCheck.Size = new System.Drawing.Size(75, 23);
+			this.btnCheck.TabIndex = 3;
+			this.btnCheck.Text = "Check";
+			this.btnCheck.UseVisualStyleBackColor = true;
+			this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.btnCheck);
+			this.Controls.Add(this.lblDisplay);
+			this.Controls.Add(this.txtAnswer);
+			this.Controls.Add(this.lblQuestion);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label lblQuestion;
+		private System.Windows.Forms.TextBox txtAnswer;
+		private System.Windows.Forms.Label lblDisplay;
+		private System.Windows.Forms.Button btnCheck;
 	}
 }
 
--- ChocolateBoxesTristanR/ChocolateBoxesTristanR/Form1.cs
+++ ChocolateBoxesTristanR/ChocolateBoxesTristanR/Form1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: First Last
+ * Created on: Day-Month-Year
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #19 - Chocolate Boxes
+ * This program...
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +22,22 @@ namespace ChocolateBoxesTristanR
 		public Form1()
 		{
 			InitializeComponent();
+			lblDisplay.Hide(); 
+		}
+
+		private void txtAnswer_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnCheck_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
