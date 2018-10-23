b56e66f0f5d4bcb4a953c3b2db64cf387139e725
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-10-23 11:06:19 -0400

    mhm

---
+++ WalkingManTristanR/WalkingManTristanR/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WalkingManTristanR
+{
+	partial class frmWalkingMan
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnStart = new System.Windows.Forms.Button();
+			this.picWalker = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.picWalker)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnStart
+			// 
+			this.btnStart.Location = new System.Drawing.Point(96, 43);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(75, 23);
+			this.btnStart.TabIndex = 0;
+			this.btnStart.Text = "START";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+			// 
+			// picWalker
+			// 
+			this.picWalker.Image = global::WalkingManTristanR.Properties.Resources.walk10;
+			this.picWalker.Location = new System.Drawing.Point(45, 72);
+			this.picWalker.Name = "picWalker";
+			this.picWalker.Size = new System.Drawing.Size(186, 177);
+			this.picWalker.TabIndex = 1;
+			this.picWalker.TabStop = false;
+			this.picWalker.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// frmWalkingMan
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.picWalker);
+			this.Controls.Add(this.btnStart);
+			this.Name = "frmWalkingMan";
+			this.Text = "WalkingMan";
+			((System.ComponentModel.ISupportInitialize)(this.picWalker)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.PictureBox picWalker;
+	}
+}
+
---
+++ WalkingManTristanR/WalkingManTristanR/Form1.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Created by: Tristan Royer
+ * Created on: 23-10-18
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #21 - Walking Man
+ * This program displays an animated man walking.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// add  the following library to use the sleep timer
+using System.Threading;
+
+namespace WalkingManTristanR
+{
+	public partial class frmWalkingMan : Form
+	{
+		public frmWalkingMan()
+		{
+			InitializeComponent();
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			// declare local variables and constants
+			const byte MAX_FRAMES = 10;
+			int picFrameCounter = 1;
+
+			//continue the loop while the frame counter has not reached the max number of frames we have to display
+			while (picFrameCounter < MAX_FRAMES + 1)
+			{
+			
+			if (picFrameCounter == 1)
+				{
+					this.picWalker.Image = Properties.Resources.walk1;
+				}
+
+				else if (picFrameCounter == 2)
+				{
+					this.picWalker.Image = Properties.Resources.walk2;
+				}
+				else if (picFrameCounter == 3)
+				{
+					this.picWalker.Image = Properties.Resources.walk3;
+				}
+				else if (picFrameCounter == 4)
+				{
+					this.picWalker.Image = Properties.Resources.walk4;
+				}
+				else if (picFrameCounter == 5)
+				{
+					this.picWalker.Image = Properties.Resources.walk5;
+				}
+				else if (picFrameCounter == 6)
+				{
+					this.picWalker.Image = Properties.Resources.walk6;
+				}
+				else if (picFrameCounter == 7)
+				{
+					this.picWalker.Image = Properties.Resources.walk7;
+				}
+				else if (picFrameCounter == 8)
+				{
+					this.picWalker.Image = Properties.Resources.walk8;
+				}
+				else if (picFrameCounter == 9)
+				{
+					this.picWalker.Image = Properties.Resources.walk9;
+				}
+				else if (picFrameCounter == 10)
+				{
+					this.picWalker.Image = Properties.Resources.walk10;
+				}
+
+			//refresh the form.This MUST be done,
+			}
+		}
+	}
+}
