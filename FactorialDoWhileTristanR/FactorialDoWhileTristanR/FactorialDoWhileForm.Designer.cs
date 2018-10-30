3bfd62caa48728ba342e5ef8b17472cdaa29e1c5
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-10-30 11:01:51 -0400

    ok

---
+++ FactorialDoWhileTristanR/FactorialDoWhileTristanR/FactorialDoWhileForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace FactorialDoWhileTristanR
+{
+	partial class FactorialDoWhileForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtUserAnswer = new System.Windows.Forms.TextBox();
+			this.Factorial = new System.Windows.Forms.ListBox();
+			this.btnCalculate = new System.Windows.Forms.Button();
+			this.lblFactorialAnswer = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// txtUserAnswer
+			// 
+			this.txtUserAnswer.Location = new System.Drawing.Point(88, 37);
+			this.txtUserAnswer.Name = "txtUserAnswer";
+			this.txtUserAnswer.Size = new System.Drawing.Size(100, 20);
+			this.txtUserAnswer.TabIndex = 0;
+			// 
+			// Factorial
+			// 
+			this.Factorial.FormattingEnabled = true;
+			this.Factorial.Location = new System.Drawing.Point(88, 104);
+			this.Factorial.Name = "Factorial";
+			this.Factorial.Size = new System.Drawing.Size(120, 95);
+			this.Factorial.TabIndex = 1;
+			// 
+			// btnCalculate
+			// 
+			this.btnCalculate.Location = new System.Drawing.Point(101, 75);
+			this.btnCalculate.Name = "btnCalculate";
+			this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+			this.btnCalculate.TabIndex = 2;
+			this.btnCalculate.Text = "Calculate";
+			this.btnCalculate.UseVisualStyleBackColor = true;
+			this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+			// 
+			// lblFactorialAnswer
+			// 
+			this.lblFactorialAnswer.AutoSize = true;
+			this.lblFactorialAnswer.Location = new System.Drawing.Point(125, 218);
+			this.lblFactorialAnswer.Name = "lblFactorialAnswer";
+			this.lblFactorialAnswer.Size = new System.Drawing.Size(13, 13);
+			this.lblFactorialAnswer.TabIndex = 3;
+			this.lblFactorialAnswer.Text = "--";
+			// 
+			// FactorialDoWhileForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.lblFactorialAnswer);
+			this.Controls.Add(this.btnCalculate);
+			this.Controls.Add(this.Factorial);
+			this.Controls.Add(this.txtUserAnswer);
+			this.Name = "FactorialDoWhileForm";
+			this.Text = "FactorialDoWhile";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtUserAnswer;
+		private System.Windows.Forms.ListBox Factorial;
+		private System.Windows.Forms.Button btnCalculate;
+		private System.Windows.Forms.Label lblFactorialAnswer;
+	}
+}
+
---
+++ FactorialDoWhileTristanR/FactorialDoWhileTristanR/FactorialDoWhileForm.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Created by: Tristan Royer
+ * Created on: 30-10-18
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #20 - Factorial Do While
+ * This program displays the factorials of a given numbers
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialDoWhileTristanR
+{
+	public partial class FactorialDoWhileForm : Form
+	{
+		public FactorialDoWhileForm()
+		{
+			InitializeComponent();
+		}
+
+		private void btnCalculate_Click(object sender, EventArgs e)
+		{
+			// declare local variables
+			double factorialAnswer;
+			double factorialNumber;
+			int factorialCounter;
+
+			// clear the items from the listbox
+			this.Factorial.Items.Clear();
+
+			//initialize the final answer by 1
+			factorialAnswer = 1;
+
+			// get the number (factorialNumber) from the user
+			factorialNumber = Convert.ToDouble(this.txtUserAnswer.Text);
+
+			//set the counter to 0
+			factorialCounter = 0;
+
+			//multiply the counter by the next incremented number until it reaches the user's number
+			do
+			{
+				//increment the counter by 1
+				factorialCounter += 1;
+
+				// list the counter number in the listbox for the user to see
+				this.Factorial.Items.Add(factorialCounter);
+				// multiply the counter by the answer
+				factorialAnswer = factorialAnswer * factorialCounter;
+
+				this.Refresh();
+
+			} while (factorialCounter < factorialNumber);
+			this.lblFactorialAnswer.Text = this.txtUserAnswer.Text + "! = " + Convert.ToString(factorialAnswer);
+		}
+	}
+}
