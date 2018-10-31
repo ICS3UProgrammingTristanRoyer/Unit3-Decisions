6bba71b0962c9a861c20d25d87eb76460c023fdc
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-10-31 11:05:43 -0400

    ok

---
+++ FactorialForLoopTristanR/FactorialForLoopTristanR/FactorialForLoopForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FactorialForLoopTristanR
+{
+	partial class FactorialForLoopForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtUserAnswer = new System.Windows.Forms.TextBox();
+			this.lstFactorial = new System.Windows.Forms.ListBox();
+			this.lblAnswer = new System.Windows.Forms.Label();
+			this.btnCalculate = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtUserAnswer
+			// 
+			this.txtUserAnswer.Location = new System.Drawing.Point(92, 27);
+			this.txtUserAnswer.Name = "txtUserAnswer";
+			this.txtUserAnswer.Size = new System.Drawing.Size(100, 20);
+			this.txtUserAnswer.TabIndex = 0;
+			this.txtUserAnswer.TextChanged += new System.EventHandler(this.txtUserAnswer_TextChanged);
+			// 
+			// lstFactorial
+			// 
+			this.lstFactorial.FormattingEnabled = true;
+			this.lstFactorial.Location = new System.Drawing.Point(81, 104);
+			this.lstFactorial.Name = "lstFactorial";
+			this.lstFactorial.Size = new System.Drawing.Size(120, 95);
+			this.lstFactorial.TabIndex = 1;
+			// 
+			// lblAnswer
+			// 
+			this.lblAnswer.AutoSize = true;
+			this.lblAnswer.Location = new System.Drawing.Point(131, 224);
+			this.lblAnswer.Name = "lblAnswer";
+			this.lblAnswer.Size = new System.Drawing.Size(13, 13);
+			this.lblAnswer.TabIndex = 2;
+			this.lblAnswer.Text = "--";
+			// 
+			// btnCalculate
+			// 
+			this.btnCalculate.Location = new System.Drawing.Point(106, 66);
+			this.btnCalculate.Name = "btnCalculate";
+			this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+			this.btnCalculate.TabIndex = 3;
+			this.btnCalculate.Text = "Calculate";
+			this.btnCalculate.UseVisualStyleBackColor = true;
+			this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+			// 
+			// FactorialForLoopForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.btnCalculate);
+			this.Controls.Add(this.lblAnswer);
+			this.Controls.Add(this.lstFactorial);
+			this.Controls.Add(this.txtUserAnswer);
+			this.Name = "FactorialForLoopForm";
+			this.Text = "FactorialForLoop";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtUserAnswer;
+		private System.Windows.Forms.ListBox lstFactorial;
+		private System.Windows.Forms.Label lblAnswer;
+		private System.Windows.Forms.Button btnCalculate;
+	}
+}
+
---
+++ FactorialForLoopTristanR/FactorialForLoopTristanR/FactorialForLoopForm.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Created by: Tristan Royer
+ * Created on: 31 - 10 - 18
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #23 - Factorial For Loop
+ * This program calculates the sum of all the numbers leading up to the value input by the user.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialForLoopTristanR
+{
+	public partial class FactorialForLoopForm : Form
+	{
+		public FactorialForLoopForm()
+		{
+			InitializeComponent();
+			lblAnswer.Hide();
+		}
+
+		private void txtUserAnswer_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnCalculate_Click(object sender, EventArgs e)
+		{
+			// declare the variables
+			int counter;
+			int sumNumber;
+			int sumAnswer;
+
+			// clear the list box
+			this.lstFactorial.Items.Clear();
+
+			// initialize the final sum to 1
+			sumAnswer = 0;
+
+			//get the value from the user
+			sumNumber = int.Parse(this.txtUserAnswer.Text);
+
+			// initizlize the counter , check to see if the counter is less than the user's number and also increments the counter.
+			for (counter = 0; counter <= sumNumber; counter += 1 )
+			{
+				// updates the sum answer
+				sumAnswer = sumAnswer + counter;
+				
+				// adds the counter's current value to the list
+				this.lstFactorial.Items.Add(counter);
+				
+				// reveals the answer label
+				lblAnswer.Show();
+
+				// Refreshes the program
+				this.Refresh();
+
+			}
+			// Updates the text to display the Sum answer.
+			this.lblAnswer.Text = (Convert.ToString(sumAnswer) + "!");
+
+
+
+		}
+	}
+}
