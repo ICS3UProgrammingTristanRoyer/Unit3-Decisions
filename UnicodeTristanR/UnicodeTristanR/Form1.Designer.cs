b2f7399db7e573d58f7394613a9e47e77518b354
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-11-01 10:59:46 -0400

    finished Unicode program

--- UnicodeTristanR/UnicodeTristanR/Form1.Designer.cs
+++ UnicodeTristanR/UnicodeTristanR/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UnicodeTristanR
 {
-	partial class Form1
+	partial class frmUnicode
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,12 +28,49 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.Unicode = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// btnStart
+			// 
+			this.btnStart.Location = new System.Drawing.Point(100, 12);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(75, 23);
+			this.btnStart.TabIndex = 0;
+			this.btnStart.Text = "Start";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+			// 
+			// Unicode
+			// 
+			this.Unicode.FormattingEnabled = true;
+			this.Unicode.Location = new System.Drawing.Point(78, 86);
+			this.Unicode.Name = "Unicode";
+			this.Unicode.Size = new System.Drawing.Size(120, 95);
+			this.Unicode.TabIndex = 1;
+			// 
+			// frmUnicode
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.Unicode);
+			this.Controls.Add(this.btnStart);
+			this.DoubleBuffered = true;
+			this.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.Name = "frmUnicode";
+			this.Text = "Unicode from A-Z";
+			this.Load += new System.EventHandler(this.frmUnicode_Load);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.ListBox Unicode;
 	}
 }
 
--- UnicodeTristanR/UnicodeTristanR/Form1.cs
+++ UnicodeTristanR/UnicodeTristanR/Form1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Tristan Royer
+ * Created on: 01-11-18
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day 24 : Unicode
+ * This program converts decimal characters to Unicode (Characters)
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +17,48 @@ using System.Windows.Forms;
 
 namespace UnicodeTristanR
 {
-	public partial class Form1 : Form
+	public partial class frmUnicode : Form
 	{
-		public Form1()
+		public frmUnicode()
 		{
 			InitializeComponent();
+		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			// declare local variables
+			int counter;
+			string aLetter;
+			const int MAX_COUNTER = 90;
+
+			
+
+			// clear the list box
+			this.Unicode.Items.Clear();
+
+			// For loop that repeats the functions until the CAPITAL letters A-Z are displayes
+			for (counter = 65; counter <= MAX_COUNTER; counter += 1)
+			{
+				// convert the value of the counter to unicode
+				aLetter = Char.ConvertFromUtf32(counter);
+
+				//adds the letter and current value of the counter to the list box
+				this.Unicode.Items.Add(aLetter + "=" + counter);
+
+				// Refreshes the form
+				this.Refresh();
+
+
+			}
+
+
+				
+
+		}
+
+		private void frmUnicode_Load(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
