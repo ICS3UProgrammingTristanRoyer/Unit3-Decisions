b8dd5581c38d2f947d9db101faa3e87e33439218
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-11-02 11:08:55 -0400

    ok

---
+++ ForEachTristanR/ForEachTristanR/ForEachForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ForEachTristanR
+{
+	partial class ForEachForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnColour = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnColour
+			// 
+			this.btnColour.Location = new System.Drawing.Point(88, 37);
+			this.btnColour.Name = "btnColour";
+			this.btnColour.Size = new System.Drawing.Size(97, 23);
+			this.btnColour.TabIndex = 0;
+			this.btnColour.Text = "Change Colour";
+			this.btnColour.UseVisualStyleBackColor = true;
+			this.btnColour.Click += new System.EventHandler(this.btnColour_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(114, 140);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "label1";
+			// 
+			// ForEachForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnColour);
+			this.Name = "ForEachForm";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnColour;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ ForEachTristanR/ForEachTristanR/ForEachForm.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by: Tristan Royer
+ * Created on: 02 - 11 - 18
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Control Objects
+ * This program cycles between random colours using new random and the colour function.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ForEachTristanR
+{
+	public partial class ForEachForm : Form
+	{
+		// allows the random function to be used
+		private Random random = new Random();
+		public ForEachForm()
+		{
+			InitializeComponent();
+		}
+
+		private void btnColour_Click(object sender, EventArgs e)
+		{
+			// declare variables 
+			int counter;
+
+			// loop through each object on the form and make it a random colour(Soruce:https://stackoverflow.com/questions/29198073/get-random-color)
+			for (counter = 0; counter <= 20; counter += 1)
+			{
+				foreach (Control aControlObject in this.Controls)
+				{
+					Color randomColor = Color.FromArgb(random.Next(0,256), random.Next(0,256), random.Next(0,256));
+
+					aControlObject.BackColor = randomColor;
+					if (aControlObject.GetType() == typeof(Label))
+					{
+						aControlObject.BackColor = randomColor;
+					}
+					this.Refresh();
+					Thread.Sleep(500);
+				}
+			}
+		}
+	}
+}
