edd9e46f84566eb2606fc6921d02ba51dafceeff
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-11-05 11:05:45 -0500

    ok

--- NestedLoopsTristanR/NestedLoopsTristanR/NestedLoopsForm.Designer.cs
+++ NestedLoopsTristanR/NestedLoopsTristanR/NestedLoopsForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NestedLoopsTristanR
 {
-	partial class Form1
+	partial class NestedLoopsForm
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,12 +28,80 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.Alphabet = new System.Windows.Forms.ListBox();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// Alphabet
+			// 
+			this.Alphabet.FormattingEnabled = true;
+			this.Alphabet.Location = new System.Drawing.Point(73, 104);
+			this.Alphabet.Name = "Alphabet";
+			this.Alphabet.Size = new System.Drawing.Size(120, 95);
+			this.Alphabet.TabIndex = 0;
+			// 
+			// btnStart
+			// 
+			this.btnStart.Location = new System.Drawing.Point(94, 64);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(75, 23);
+			this.btnStart.TabIndex = 1;
+			this.btnStart.Text = "Start";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+			this.menuStrip1.TabIndex = 2;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			// 
+			// NestedLoopsForm
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.btnStart);
+			this.Controls.Add(this.Alphabet);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "NestedLoopsForm";
+			this.Text = "NestedLoops";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ListBox Alphabet;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
 	}
 }
 
--- NestedLoopsTristanR/NestedLoopsTristanR/NestedLoopsForm.cs
+++ NestedLoopsTristanR/NestedLoopsTristanR/NestedLoopsForm.cs
@@ -10,11 +10,38 @@ using System.Windows.Forms;
 
 namespace NestedLoopsTristanR
 {
-	public partial class Form1 : Form
+	public partial class NestedLoopsForm : Form
 	{
-		public Form1()
+		public NestedLoopsForm()
 		{
 			InitializeComponent();
 		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			//declare local variables
+			int smallCounter;
+			int capitalCounter;
+			string firstLetter;
+			string secondLetter;
+			const int MAX_COUNTER = 90;
+
+			//clear the list box
+			this.Alphabet.Items.Clear();
+
+			// For loop that executes until  the nested if statement has executed fully.
+			for (capitalCounter = 65; capitalCounter <= MAX_COUNTER; capitalCounter += 1)
+			{
+				firstLetter = Char.ConvertFromUtf32(capitalCounter);
+
+				for (smallCounter = 95; smallCounter <= MAX_COUNTER; smallCounter++)
+				{ //convert the value of the counter to unicode
+					secondLetter = Char.ConvertFromUtf32(smallCounter);
+					this.Alphabet.Items.Add(firstLetter + "->" + secondLetter);
+				}
+
+			}
+		}
 	}
 }
+
